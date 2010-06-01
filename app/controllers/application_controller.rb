d35d177b83da39789dcf38b45241985124e23474
Tom Lieber <tom@alltom.com>
2010-05-31 20:03:14 -0700

    authenticate with basic HTTP

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,16 @@
 class ApplicationController < ActionController::Base
   helper :all
   protect_from_forgery
+  
+  before_filter :verify_access
+  
+  def verify_access
+    authenticate_or_request_with_http_basic("App Realm") do |username, password|
+      if ENV["APP_USER"] && ENV["APP_SECRET"]
+        ENV["APP_USER"] == username && ENV["APP_SECRET"] == password
+      else
+        false
+      end
+    end
+  end
 end
